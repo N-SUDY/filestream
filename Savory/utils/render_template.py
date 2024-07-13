8128eb9e04fbb31757d374930c0c5be3c29f9689
Sachin Anand <98984936+Sachinanand99@users.noreply.github.com>
2024-07-13 15:44:40 +0530

    removed unneccesary stuffs

--- Savory/utils/render_template.py
+++ Savory/utils/render_template.py
@@ -50,14 +50,6 @@ async def render_page(id, secure_hash):
         <button style="font-size: 20px; background-color: yellow; border-radius: 10px;" onclick="window.location.href = '{current_url}'">Save in your gallery</button> &nbsp
     </center>
 </p>
-</p>
-<center>
-    <h2>
-        <a href="https://telegram.dog/+kc6bYRCsWdlhOTI1">
-            <img src="https://graph.org/file/b57cdba982191a25db535.jpg" alt="Rkbotz" width="150" height="75">
-        </a>
-    </h2>
-</center>
 
 '''
 
